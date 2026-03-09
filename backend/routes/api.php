5808f0c4d46d247920178b209b3adbcadaf1dce7
KArtem <k6art5@gmail.com>
2026-03-09 22:43:31 +0700

    added routes for auth: login, register, me and logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserResourceController;
@@ -8,6 +9,16 @@ use App\Http\Controllers\PostResourceController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function (): void {
+        Route::post('me', [AuthController::class, 'me']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
 
 Route::resources([
     'users' => UserResourceController::class,
