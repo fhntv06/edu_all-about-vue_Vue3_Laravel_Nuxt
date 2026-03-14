a6e0ea216a0d8d7e1fe77c8bcb4390c196b1fd80
KArtem <k6art5@gmail.com>
2026-03-14 18:10:38 +0700

    create new vars in .env

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,9 +1,10 @@
-const apiConfig = {
-  baseURL: import.meta.env.VITE_APP_URL_API,
+export default {
+  protocol: import.meta.env.VITE_APP_API_PROTOCOL,
+  host: import.meta.env.VITE_APP_API_HOST,
+  port: import.meta.env.VITE_APP_API_PORT,
+  baseURL: `${import.meta.env.VITE_APP_API_PROTOCOL}://${import.meta.env.VITE_APP_API_HOST}:${import.meta.env.VITE_APP_API_PORT}`,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   }
 }
-
-export default apiConfig
