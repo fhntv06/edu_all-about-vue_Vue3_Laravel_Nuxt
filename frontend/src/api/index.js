fe754ffcdab0fcd37f0aa248807c2b622f41cbe3
KArtem <k6art5@gmail.com>
2026-03-15 17:56:10 +0700

    create api for auth imports

---
+++ frontend/src/api/index.js
@@ -0,0 +1,21 @@
+import { fetchLogin, fetchRegister, fetchLogout, fetchMe } from './auth'
+
+export {
+  // Auth methods
+  fetchLogin,
+  fetchRegister,
+  fetchLogout,
+  fetchMe
+
+  // Posts methods
+    // fetchGetPosts
+    // fetchUpdatePost
+    // fetchDeletePost
+    // fetchCreatePost
+
+  // Users methods
+    // fetchGetUsers
+    // fetchUpdateUser
+    // fetchDeleteUser
+    // fetchCreateUser
+}
