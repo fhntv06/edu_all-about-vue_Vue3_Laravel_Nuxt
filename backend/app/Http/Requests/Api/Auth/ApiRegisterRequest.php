04201eac9b39a7c049ea9063169ef85eae5f0018
KArtem <k6art5@gmail.com>
2026-03-15 18:02:50 +0700

    backend / correct rules and messages for fields in ApiRegisterRequest for frontend

--- backend/app/Http/Requests/Api/Auth/ApiRegisterRequest.php
+++ backend/app/Http/Requests/Api/Auth/ApiRegisterRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class ApiRegisterRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,9 +14,75 @@ class ApiRegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
+            'firstname' => 'required|string|max:255|regex:/^[a-zA-Z]+$/',
+            'login' => 'required|string|max:255|regex:/^[a-zA-Z0-9_]+$/|unique:users',
+            'email' => 'required|string|regex:/^[^\s@]+@[^\s@]+\.[^\s@]+$/|max:255|unique:users',
+            'phone' => 'string|min:11|max:16|regex:/^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/|unique:users',
+            'password' => 'required|string|min:8|max:255',
+            'privacyAccepted' => 'required|bool'
+        ];
+    }
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'firstname' => [
+                'required' => 'Поле ":attribute" обязательно для заполнения!',
+                'regex' => 'Поле ":attribute" должно содержать только буквы латинского алфавита!',
+                'max' => 'Поле ":attribute" имеет слишком длинное значение!',
+                'string' => 'Поле ":attribute" имеет значение с неверным типом данных!'
+            ],
+            'login' => [
+                'required' => 'Поле ":attribute" обязательно для заполнения!',
+                'unique' => 'Поле ":attribute" с таким значением уже существует!',
+                'regex' => 'Поле ":attribute" должно содержать только буквы латинского алфавита, цифры или знак "_"!',
+                'max' => 'Поле ":attribute" имеет слишком длинное значение!',
+                'string' => 'Поле ":attribute" имеет значение с неверным типом данных!'
+            ],
+            'email' => [
+                'required' => 'Поле ":attribute" обязательно для заполнения!',
+                'unique' => 'Поле ":attribute" с таким значением уже существует!',
+                'regex' => 'Поле ":attribute" имеет не корректное значение!',
+                'max' => 'Поле ":attribute" имеет слишком длинное значение!',
+                'string' => 'Поле ":attribute" имеет значение с неверным типом данных!'
+            ],
+            'phone' => [
+                'unique' => 'Поле ":attribute" с таким значением уже существует!',
+                'regex' => 'Поле ":attribute" имеет не корректный формат!',
+                'min' => 'Поле ":attribute" имеет слишком короткое значение!',
+                'max' => 'Поле ":attribute" имеет слишком длинное значение!',
+                'string' => 'Поле ":attribute" имеет значение с неверным типом данных!'
+            ],
+            'password' => [
+                'required' => 'Поле ":attribute" обязательно для заполнения!',
+                'min' => 'Поле ":attribute" должно содержать минимум 8 символов!',
+                'max' => 'Поле ":attribute" имеет слишком длинное значение!',
+                'string' => 'Поле ":attribute" имеет значение с неверным типом данных!'
+            ],
+            'privacyAccepted' => [
+                'required' => 'Поле ":attribute" обязательно для заполнения!',
+                'bool' => 'Поле ":attribute" имеет значение с неверным типом данных!'
+            ]
+        ];
+    }
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'firstname' => 'Имя',
+            'login' => 'Логин',
+            'phone' => 'Телефон',
+            'email' => 'Email',
+            'password' => 'Пароль',
+            'privacyAccepted' => 'Согласие на обработку персональных данных'
         ];
     }
 }
