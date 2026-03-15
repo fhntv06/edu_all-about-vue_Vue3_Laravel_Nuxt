aee24992c45e28bb7135f875bbede61618d91301
KArtem <k6art5@gmail.com>
2026-03-15 18:02:27 +0700

    backend / correct rules and messages for fields in ApiLoginRequest for frontend

--- backend/app/Http/Requests/Api/Auth/ApiLoginRequest.php
+++ backend/app/Http/Requests/Api/Auth/ApiLoginRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class ApiLoginRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,9 +14,47 @@ class ApiLoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
+            'email' => 'required|string|regex:/^[^\s@]+@[^\s@]+\.[^\s@]+$/|max:255',
+            'password' => 'required|string|min:8|max:255',
+            'rememberMe' => 'bool'
+        ];
+    }
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email' => [
+                'required' => 'Поле ":attribute" не может быть пустым!',
+                'string' => 'Поле ":attribute" имеет значение с неверным типом данных!',
+                'regex' => 'Поле ":attribute" должно быть корректным email адресом!',
+                'max' => 'Поле ":attribute" не должно превышать :max символов!',
+            ],
+            'password' => [
+                'required' => 'Поле ":attribute" не может быть пустым!',
+                'string' => 'Поле ":attribute" имеет значение с неверным типом данных!',
+                'min' => 'Поле ":attribute" должно содержать минимум :min символов!',
+                'max' => 'Поле ":attribute" не должно превышать :max символов!',
+            ],
+            'rememberMe' => [
+                'bool' => 'Поле ":attribute" имеет значение с неверным типом данных!'
+            ],
+        ];
+    }
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'email' => 'Email',
+            'password' => 'Пароль',
+            'rememberMe' => 'Запомнить меня',
         ];
     }
 }
