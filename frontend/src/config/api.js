9563729045a0ad8a65e9f9c7f11cd0a05ac6bdd6
KArtem <k6art5@gmail.com>
2026-03-15 17:58:32 +0700

    change baseURL param in config api

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,7 +2,7 @@ export default {
   protocol: import.meta.env.VITE_APP_API_PROTOCOL,
   host: import.meta.env.VITE_APP_API_HOST,
   port: import.meta.env.VITE_APP_API_PORT,
-  baseURL: `${import.meta.env.VITE_APP_API_PROTOCOL}://${import.meta.env.VITE_APP_API_HOST}:${import.meta.env.VITE_APP_API_PORT}`,
+  baseURL: `${import.meta.env.VITE_APP_API_PROTOCOL}://${import.meta.env.VITE_APP_API_HOST}:${import.meta.env.VITE_APP_API_PORT}/api`,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
