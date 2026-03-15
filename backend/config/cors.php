5da306fcd29d939dc5066e3c0c8b3ba2f70da00d
KArtem <k6art5@gmail.com>
2026-03-15 18:00:35 +0700

    backend / correct cors rules

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'http://localhost:5173'],
+    'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', 'http://localhost:5173')),
 
     'allowed_origins_patterns' => [],
 
