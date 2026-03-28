b7b7d0846bdf2dafdd7fb71a3a23fa463529475b
KArtem <k6art5@gmail.com>
2026-03-28 14:07:51 +0700

    backend / events amd listeners registered in provider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,10 +2,26 @@
 
 namespace App\Providers;
 
+use App\Events\Post\PostCreated;
+use App\Events\Post\PostGetted;
+use App\Events\Post\PostUpdated;
+use App\Events\User\UserCreated;
+use App\Listeners\SendNotificaionForPostCreated;
+use App\Listeners\SendNotificaionForPostUpdated;
+use App\Listeners\SendNotificaionForUserCreated;
+use App\Listeners\SendNotificationForPost;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public array $registers = [
+        PostGetted::class => SendNotificationForPost::class,
+        PostCreated::class => SendNotificaionForPostCreated::class,
+        PostUpdated::class => SendNotificaionForPostUpdated::class,
+        UserCreated::class => SendNotificaionForUserCreated::class,
+    ];
+
     /**
      * Register any application services.
      */
@@ -19,6 +35,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerEventsAndListeners();
+    }
+
+    private function registerEventsAndListeners(): void
+    {
+        foreach ($this->registers as $event => $listener) {
+            Event::listen($event, $listener);
+        }
     }
 }
